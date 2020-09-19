c442501ef9a0896c29201cba0063dfdbe547d024
Ashish <as7220004@gmail.com>
2020-09-19 23:28:15 +0530

    Slider and number input synced.

---
+++ index.js
@@ -0,0 +1,12 @@
+//jshint esversion: 9
+const characterAmountRange = document.getElementById('characterAmountRange');
+const characterAmountNumber = document.getElementById('characterAmountNumber');
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount);
+characterAmountRange.addEventListener('input', syncCharacterAmount);
+
+function syncCharacterAmount(e){
+    const value = e.target.value;
+    characterAmountNumber.value = value;
+    characterAmountRange.value = value;
+}
